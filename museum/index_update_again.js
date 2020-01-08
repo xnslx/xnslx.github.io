f67572a03424c091f56bb68cac2d1e23d6e2bca4
Xian Li <xnslx89@gmail.com>
2020-01-07 23:11:29 -0600

    Create index_update_again.js

---
+++ museum/index_update_again.js
@@ -0,0 +1,29 @@
+document.getElementById("toggleBtn").addEventListener('click', () => {
+    toggleBtn.classList.toggle('active');
+    document.getElementsByClassName("wrapper").classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+// var fixed = document.getElementById('menu_container');
+// fixed.addEventListener('touchmove', function(e) {
+
+//     e.preventDefault();
+
+// }, false);
+
+
+
+// function noscroll() {
+//     window.scrollTo(0, 0);
+// }
+
+// window.addEventListener("scroll", noscroll);
